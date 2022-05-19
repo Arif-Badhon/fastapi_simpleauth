ee8263dcd3819b3170f19954b54299c31a21c8cf
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-05-19 15:39:48 +0600

    updated authentication and administration

--- app/api.py
+++ app/api.py
@@ -1,6 +1,10 @@
 # app/api.py
-from fastapi import FastAPI
-from app.model import PostSchema
+
+from fastapi import FastAPI, Body, Depends
+
+from app.model import PostSchema, UserSchema, UserLoginSchema
+from app.auth.auth_bearer import JWTBearer
+from app.auth.auth_handler import signJWT
 
 posts = [
     {
@@ -35,10 +39,30 @@ def single_post(id: int) -> dict:
                 "data": post
             }
 
-@app.post("/posts", tags=["posts"])
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
 def add_post(post:PostSchema) -> dict:
     post.id = len(posts) + 1
     posts.append(post.dict())
     return{
         "data" : "post added"
     }
+
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema=Body(...)):
+    users.append(user)
+    return signJWT(user.email)
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return{
+        "error": "wrong login details"
+    }
+
